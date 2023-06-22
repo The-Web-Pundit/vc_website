7ec58e1d7e769ce6d50d651b0f0d2ae8162418d2
BaddelaManikanta <baddelamanikanta@gmail.com>
2023-06-22 09:58:25 +0530

    updated

---
+++ src/api/tesing/controllers/tesing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tesing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tesing.tesing');
---
+++ src/api/tesing/services/tesing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tesing service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tesing.tesing');
