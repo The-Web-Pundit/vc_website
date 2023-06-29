222f592dc5a011e6902d03c8f0d06abc685352d8
BaddelaManikanta <baddelamanikanta@gmail.com>
2023-06-29 12:46:41 +0530

    email config added

--- src/api/pitch/content-types/pitch/lifecycles.js
+++ src/api/pitch/content-types/pitch/lifecycles.js
@@ -6,7 +6,7 @@ module.exports = {
           to: "hello@z3partners.co",
           from: "baddelamanikanta2@gmail.com",
           replyTo: "baddelamanikanta2@gmail.com",
-          subject: "New Pitch form submission from Coverme Website",
+          subject: "New Pitch form submission from Z3Partners Website",
           html: `
                 Name :  ${result.Name}  <br/>
                 Email : ${result.Email}   <br/>
