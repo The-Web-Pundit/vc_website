3ecc0c752f816a1526888d2c5de9ddc2be7def7b
prasad.r@pacewisdom.com <prasad.r@pacewisdom.com>
2023-08-09 13:30:40 +0530

    updated plugin aws

--- config/plugins.js
+++ config/plugins.js
@@ -2,19 +2,16 @@ module.exports = ({ env }) => (
   {
   // ...
   upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+    provider: 'aws-s3',
+    providerOptions: {
+    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+    region: env('AWS_REGION'),
+    params: {
+    Bucket: env('AWS_BUCKET_NAME'),
     },
   },
+     },
   // ...
 
   ckeditor: {
