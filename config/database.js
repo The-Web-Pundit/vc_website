36791a814eff73955cec93bb504112c5232cd81b
prasad.r@pacewisdom.com <prasad.r@pacewisdom.com>
2023-08-11 16:52:21 +0530

    updated aws plugin

--- config/database.js
+++ config/database.js
@@ -2,15 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '65.0.85.33'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      database: env('DATABASE_NAME', 'z3'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
     debug: false,
   },
--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,20 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'z3-dev.s3.ap-south-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'z3-dev.s3.ap-south-1.amazonaws.com',
+          ],
           upgradeInsecureRequests: null,
         },
       },
