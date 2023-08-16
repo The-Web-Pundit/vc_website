36cd1a4144b9d61ee11dce6e6b9a22351df0124e
rajkumarm95 <rajkumar.murugesan@zysk.tech>
2023-08-16 18:11:17 +0530

    update : ACl for S3 added

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
     debug: false,
   },
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,37 +1,42 @@
-module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'z3-dev.s3.ap-south-1.amazonaws.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'z3-dev.s3.ap-south-1.amazonaws.com',
-          ],
-          upgradeInsecureRequests: null,
+    // ~/strapi-aws-s3/backend/config/middlewares.js
+
+    module.exports = [
+      'strapi::errors',
+      /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'z-3-dev.s3.ap-south-1.amazonaws.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'z-3-dev.s3.ap-south-1.amazonaws.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
         },
       },
-    },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+      /* End of snippet */
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
