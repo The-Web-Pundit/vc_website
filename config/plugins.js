b5d6d91fa349e92ddd9b59baaed15ee9371adf31
GAMATAM VAJINTHA <gamatam.vajintha@zysk.tech>
2023-09-26 18:16:45 +0530

    files chenged

--- config/plugins.js
+++ config/plugins.js
@@ -6,13 +6,13 @@ module.exports = ({ env }) => (
       provider: 'aws-s3',
       providerOptions: {
         s3Options:{
-        accessKeyId: env('AWS_ACCESS_KEY_ID','AKIAXEXE3STWPI6YITK6'),
-        secretAccessKey: env('AWS_ACCESS_SECRET','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'),
-        region: env('AWS_REGION','ap-south-1'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
         params: {
           ACL: env('AWS_ACL', 'public-read'),
           signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('AWS_BUCKET','z-3-dev'),
+          Bucket: env('AWS_BUCKET'),
         },
       },
     },
